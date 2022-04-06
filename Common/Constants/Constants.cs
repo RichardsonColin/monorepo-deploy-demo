9b2aaf92321416f537ea37bf95bab3789f71d5be
Colin Richardson <colin.richardson.dev@gmail.com>
2022-04-06 17:17:46 -0500

    initial commit

---
+++ Common/Constants/Constants.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Leadosaurus.Common
+{
+  public static class Constants
+  {
+    public const string AirTableApiUrl = $"{Http}api.airtable.com/v0/";
+    private const string Http = "https://";
+  }
+}
